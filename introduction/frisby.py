7471680d1dfa4c16f67a73072b816f8f434c15e4
Cameron <axxes9mc@gmail.com>
2021-10-01 11:13:31 -0600

    Added color to frisby.py text

--- introduction/frisby.py
+++ introduction/frisby.py
@@ -3,6 +3,6 @@ def frisby_intro():
     number = random.randint(0, 5)
     greetings = ["Hello there!", "Welcome to the jungle!", "This is a normal introduction", "Why hello there, didn't expect you to be here", "Hola! Como Esta! I don't know Spanish!", "Hello, this is a function that was called by a file named somthing."]
 
-    print(greetings[number])
+    print(f"\033[1;31;40m {greetings[number]}")
 
 frisby_intro()
