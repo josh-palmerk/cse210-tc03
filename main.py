672e8ef1b4e7b6691abe87533e5caa65e6aa1c82
JosephCheney <che20001@byui.edu>
2021-10-01 10:56:49 -0600

    Added Hello from joseph function

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ Author: Bullet Hell Gang
 """
 
 import introduction.hatton
+import introduction.cheney as cheney
 
 def main():
     print("Welcome to our collaborative program.")
     introduction.hatton.dallins_hello()
+    cheney.hello()
 
 
 if __name__ == "__main__":
